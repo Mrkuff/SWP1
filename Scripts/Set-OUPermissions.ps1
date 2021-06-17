6cd888b3a36e5e37b6475ecccae308aedd5874ab
Mrkuff <kuff_@hotmail.com>
2021-06-17 17:56:24 -0400

    Update Set-OUPermissions.ps1
    Instead of using dsacls, here's a shiny Powershell script for delegating Join Domain permission to OU.  This can be use as inpiration for any kind of delagation scripting!  Shiny!

--- Scripts/Set-OUPermissions.ps1
+++ Scripts/Set-OUPermissions.ps1
@@ -1,54 +1,131 @@
-﻿<#
-Created:	 2013-01-08
-Version:	 1.0
-Author       Mikael Nystrom and Johan Arwidmark       
-Homepage:    http://www.deploymentfundamentals.com
+function Set-ADPremissionJoinDomain{
+<#
+.DESCRIPTION
+The function will delegate the permission for a group to join a computer to a domain 
 
-Disclaimer:
-This script is provided "AS IS" with no warranties, confers no rights and 
-is not supported by the authors or DeploymentArtist.
-
-Author - Mikael Nystrom
-    Twitter: @mikael_nystrom
-    Blog   : http://deploymentbunny.com
-
-Author - Johan Arwidmark
-    Twitter: @jarwidmark
-    Blog   : http://deploymentresearch.com
+.EXAMPLE
+Set-ADPremissionJoinDomain -Group "Delegated Users" -LDAPPath "ou=Mobile,ou=Computers,ou=Test2,ou=!Offices,dc=corpnet,dc=liox,dc=org"
 #>
+   [CmdletBinding()]
+   param(
+      [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0)]
+      [ValidateNotNullOrEmpty()]
+      [System.String]
+      $Group,
+      
+      [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=1)]
+      [ValidateNotNullOrEmpty()]
+      [System.String]
+      $LDAPPath
+   )
+   begin {
+      try {
+         import-module activedirectory
+         $guidNull                       = new-object Guid 00000000-0000-0000-0000-000000000000
+         $guidComputerObject             = new-object Guid bf967a86-0de6-11d0-a285-00aa003049e2
+         $guidUserAccountRestrictions    = new-object Guid 4c164200-20c0-11d0-a768-00aa006e0529
+         $guidServicePrincipalName       = new-object Guid f3a64788-5306-11d1-a9c5-0000f80367c1
+         $guidDNSHostName                = new-object Guid 72e39547-7b18-11d1-adef-00c04fd8d5cd
+         $guidUserForceChangePassword    = new-object Guid 00299570-246d-11d0-a768-00aa006e0529
+       
+      } 
+      catch {
+      }
+   }
+   process {
+      try {
+         # The First Part of the exercise will be to collect the SID for the Group we will be delegating to
+         $groupObject = Get-ADGroup $Group
+         $groupSID = new-object System.Security.Principal.SecurityIdentifier $groupObject.SID
+         
+         #Now we will link to the OU Object
+         $ADObject = [ADSI]("LDAP://" + $LDAPPath)
+         
+         # Next we are going to create an Access Control Entry
+ 
+         # Create/delete Computer Objects
+         # PropagationFlags      : None
+         # InheritanceFlags      : ContainerInherit
+         # ObjectType            : bf967a86-0de6-11d0-a285-00aa003049e2
+         # InheritanceType       : All
+         # InheritedObjectType   : 00000000-0000-0000-0000-000000000000
+         # ObjectFlags           : ObjectAceTypePresent
+         # ActiveDirectoryRights : CreateChild, DeleteChild
+         # 
+         $ace = new-object System.DirectoryServices.ActiveDirectoryAccessRule $groupSID,"CreateChild, DeleteChild","Allow",$guidComputerObject,"All",$guidNull
+         $ADObject.ObjectSecurity.AddAccessRule($ace)
+         
+         
+         # Read and write Account Restrictions
+         # PropagationFlags      : InheritOnly
+         # InheritanceFlags      : ContainerInherit
+         # ObjectType            : 4c164200-20c0-11d0-a768-00aa006e0529
+         # InheritanceType       : Descendents
+         # InheritedObjectType   : bf967a86-0de6-11d0-a285-00aa003049e2
+         # ObjectFlags           : ObjectAceTypePresent, InheritedObjectAceTypePresent
+         # ActiveDirectoryRights : ReadProperty, WriteProperty
+ 
+         $ace = new-object System.DirectoryServices.ActiveDirectoryAccessRule $groupSID,"ReadProperty, WriteProperty","Allow",$guidUserAccountRestrictions,"Descendents",$guidComputerObject
+         $ADObject.ObjectSecurity.AddAccessRule($ace)
+         
+         # Validated write and write to service principal name
+         # PropagationFlags      : InheritOnly
+         # InheritanceFlags      : ContainerInherit
+         # ObjectType            : f3a64788-5306-11d1-a9c5-0000f80367c1
+         # InheritanceType       : Descendents
+         # InheritedObjectType   : bf967a86-0de6-11d0-a285-00aa003049e2
+         # ObjectFlags           : ObjectAceTypePresent, InheritedObjectAceTypePresent
+         # ActiveDirectoryRights : Self
+ 
+         $ace = new-object System.DirectoryServices.ActiveDirectoryAccessRule $groupSID,"Self, WriteProperty","Allow",$guidServicePrincipalName,"Descendents",$guidComputerObject
+         $ADObject.ObjectSecurity.AddAccessRule($ace)
 
-Param
-(
-[parameter(mandatory=$true,HelpMessage="Please, provide a name.")][ValidateNotNullOrEmpty()]$Account,
-[parameter(mandatory=$true,HelpMessage="Please, provide the organization unit to be used.")][ValidateNotNullOrEmpty()]$TargetOU
-)
+         # Validated write and Read and write DNS host name attributes
+         # PropagationFlags      : InheritOnly
+         # InheritanceFlags      : ContainerInherit
+         # ObjectType            : 72e39547-7b18-11d1-adef-00c04fd8d5cd
+         # InheritanceType       : Descendents
+         # InheritedObjectType   : bf967a86-0de6-11d0-a285-00aa003049e2
+         # ObjectFlags           : ObjectAceTypePresent, InheritedObjectAceTypePresent
+         # ActiveDirectoryRights : Self
+ 
+         $ace = new-object System.DirectoryServices.ActiveDirectoryAccessRule $groupSID,"Self, ReadProperty, WriteProperty","Allow",$guidDNSHostName,"Descendents",$guidComputerObject
+         $ADObject.ObjectSecurity.AddAccessRule($ace)
 
-# Start logging to screen
-Write-host (get-date -Format u)" - Starting"
+         # Reset Password
+         # PropagationFlags      : InheritOnly
+         # InheritanceFlags      : ContainerInherit
+         # ObjectType            : 00299570-246d-11d0-a768-00aa006e0529
+         # InheritanceType       : Descendents
+         # InheritedObjectType   : bf967a86-0de6-11d0-a285-00aa003049e2
+         # ObjectFlags           : ObjectAceTypePresent, InheritedObjectAceTypePresent
+         # ActiveDirectoryRights : ExtendedRight
+ 
+         $ace = new-object System.DirectoryServices.ActiveDirectoryAccessRule $groupSID,"ExtendedRight","Allow",$guidUserForceChangePassword,"Descendents",$guidComputerObject
+         $ADObject.ObjectSecurity.AddAccessRule($ace)
 
-# This i what we typed in
-Write-host "Account to search for is" $Account
-Write-Host "OU to search for is" $TargetOU
-
-$CurrentDomain = Get-ADDomain
-
-$OrganizationalUnitDN = $TargetOU+","+$CurrentDomain
-$SearchAccount = Get-ADUser $Account
-
-$SAM = $SearchAccount.SamAccountName
-$UserAccount = $CurrentDomain.NetBIOSName+"\"+$SAM
-
-Write-Host "Account is = $UserAccount"
-Write-host "OU is =" $OrganizationalUnitDN
-
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":CCDC;Computer" /I:T | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":LC;;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":RC;;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":WD;;Computer" /I:S  | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":WP;;Computer" /I:S  | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":RP;;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Reset Password;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Change Password;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to service principal name;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to DNS host name;Computer" /I:S | Out-Null
-dsacls.exe $OrganizationalUnitDN
+         # Write all properties
+         # PropagationFlags      : InheritOnly
+         # InheritanceFlags      : ContainerInherit
+         # ObjectType            : 00000000-0000-0000-0000-000000000000
+         # InheritanceType       : Descendents
+         # InheritedObjectType   : bf967a86-0de6-11d0-a285-00aa003049e2
+         # ObjectFlags           : InheritedObjectAceTypePresent
+         # ActiveDirectoryRights : WriteProperty
+ 
+         $ace = new-object System.DirectoryServices.ActiveDirectoryAccessRule $groupSID,"WriteProperty","Allow",$guidNull,"Descendents",$guidComputerObject
+         $ADObject.ObjectSecurity.AddAccessRule($ace)
+         
+         $ADObject.CommitChanges()
+ 
+      }
+      catch {
+      }
+   }
+   end {
+      try {
+      }
+      catch {
+      }
+   }
+}
